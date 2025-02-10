aaeb7b3dadbe1130bd3fed64960feefb218e7b85
Le Vivilet <levivlet@pm.me>
2025-02-10 22:16:05 +0100

    feature: add input

--- packages/extension/media/index.js
+++ packages/extension/media/index.js
@@ -3,7 +3,20 @@
 const initialize = async () => {
   const app = document.createElement('div')
   app.className = 'App'
-  app.textContent = 'hello world'
+
+  const output = document.createElement('div')
+  output.className = 'Output'
+
+  const form = document.createElement('form')
+  form.className = 'Form'
+  const input = document.createElement('textarea')
+  input.className = 'Input'
+
+  const button = document.createElement('button')
+  button.type = 'submit'
+  button.textContent = 'Send'
+  form.append(input, button)
+  app.append(output, form)
 
   document.body.append(app)
   return {}

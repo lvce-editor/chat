02b209493667044a8ad30dd11f6e4449fb443cb8
Le Vivilet <levivlet@pm.me>
2025-02-10 22:04:23 +0100

    static

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -20,36 +20,27 @@ const { commitHash } = await exportStatic({
   root,
 })
 
-await cp(path.join(root, 'dist2'), path.join(root, 'dist', commitHash, 'extensions', 'builtin.video-preview'), {
+await cp(path.join(root, 'dist2'), path.join(root, 'dist', commitHash, 'extensions', 'builtin.chat'), {
   recursive: true,
   force: true,
 })
 
 await replace({
   path: path.join(root, 'dist', commitHash, 'config', 'webExtensions.json'),
-  occurrence: 'src/videoPreviewMain.ts',
-  replacement: 'dist/videoPreviewMain.js',
+  occurrence: 'src/chatMain.ts',
+  replacement: 'dist/chatMain.js',
 })
 await replace({
   path: path.join(root, 'dist', commitHash, 'config', 'webExtensions.json'),
-  occurrence: '../video-preview-worker/src/videoPreviewWorkerMain.ts',
-  replacement: './video-preview-worker/dist/videoPreviewWorkerMain.js',
+  occurrence: '../chat-worker/src/chatWorkerMain.ts',
+  replacement: './chat-worker/dist/chatWorkerMain.js',
 })
 
-const pathPrefix = '/video-preview'
+const pathPrefix = '/chat'
 const webViewsPath = join(root, 'dist', commitHash, 'config', 'webViews.json')
-const extensionJsonPath = join(root, 'dist', commitHash, 'extensions', 'builtin.video-preview', 'extension.json')
+const extensionJsonPath = join(root, 'dist', commitHash, 'extensions', 'builtin.chat', 'extension.json')
 const extensionJsonContent = await readFile(extensionJsonPath, 'utf8')
 const extensionJson = JSON.parse(extensionJsonContent)
 extensionJson.webViews[0].path = `${commitHash}/extensions/${extensionJson.id}/${extensionJson.webViews[0].path}`
 extensionJson.webViews[0].remotePath = `${pathPrefix}/${commitHash}/extensions/${extensionJson.id}`
 await writeFile(webViewsPath, JSON.stringify(extensionJson.webViews, null, 2) + '\n')
-
-await rm(join(root, 'dist', commitHash, 'playground'), { recursive: true, force: true })
-await cp(join(root, 'packages', 'sample-files', 'files'), join(root, 'dist', commitHash, 'playground'), {
-  recursive: true,
-})
-
-const dirents = await readdir(join(root, 'dist', commitHash, 'playground'))
-const fileMap = dirents.map((dirent) => `/playground/${dirent}`)
-await writeFile(join(root, 'dist', commitHash, 'config', 'fileMap.json'), JSON.stringify(fileMap, null, 2) + '\n')

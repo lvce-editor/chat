fac1e6a85e50cafd5c2eb0274af2acedb66cea5e
Le Vivilet <levivlet@pm.me>
2025-02-10 22:59:18 +0100

    feature: query real response from api

--- packages/chat-worker/src/parts/Create/Create.ts
+++ packages/chat-worker/src/parts/Create/Create.ts
@@ -4,11 +4,18 @@ import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 export const create = async ({ port, savedState, webViewId, uri, id }) => {
   const apiKey = await globalThis.rpc.invoke('WebView.getSecret', 'secrets.claude')
 
+  // TODO make these configurable
+  const modelId = 'claude-3-5-haiku-20241022'
+  const url = 'https://api.anthropic.com/v1/messages'
+  const anthropicVersion = '2023-06-01'
+
   const webView: WebView = {
-    url: '',
     time: 0,
     port,
     apiKey,
+    modelId,
+    url,
+    anthropicVersion,
   }
   WebViewStates.set(id, webView)
   await port.invoke('initialize')

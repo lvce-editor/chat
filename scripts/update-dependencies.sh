151220126c0dca0d4b1dfe644ecf61f230580eb8
Le Vivilet <levivlet@pm.me>
2025-02-11 19:55:04 +0100

    update dependencies

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -30,9 +30,8 @@ function updateDependencies {
 cd packages/build                && updateDependencies && cd ../../ &&
 cd packages/e2e                  && updateDependencies && cd ../../ &&
 cd packages/extension            && updateDependencies && cd ../../ &&
-cd packages/integration          && updateDependencies && cd ../../ &&
 cd packages/server               && updateDependencies && cd ../../ &&
-cd packages/video-preview-worker && updateDependencies && cd ../../ &&
+cd packages/chat-worker          && updateDependencies && cd ../../ &&
 
 echo "Great Success!"
 

8708370be62dd930aa457799ea37e844620ee30d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-11 19:43:23 +0100

    feature: stream chat responses (#23)

--- packages/chat-worker/src/parts/GetChatResponse/GetChatResponse.ts
+++ packages/chat-worker/src/parts/GetChatResponse/GetChatResponse.ts
@@ -1,32 +1,35 @@
-import * as GetResponseString from '../GetResponseString/GetResponseString.ts'
-
-export const getChatResponse = async (message, apiKey, modelId, url, anthropicVersion) => {
+export const getChatResponse = async (
+  message: string,
+  apiKey: string,
+  modelId: string,
+  url: string,
+  anthropicVersion: string,
+  stream: boolean,
+): Promise<ReadableStream<Uint8Array<ArrayBufferLike>>> => {
   // TODO move networking into separate web worker
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'x-api-key': apiKey,
-        'anthropic-version': anthropicVersion,
-        'content-type': 'application/json',
-        'anthropic-dangerous-direct-browser-access': 'true', // https://github.com/anthropics/anthropic-sdk-typescript/pull/504#issuecomment-2306060472
-      },
-      body: JSON.stringify({
-        model: modelId,
-        max_tokens: 1024,
-        messages: [{ role: 'user', content: message }],
-      }),
-    })
-    if (!response.ok) {
-      if (response.status === 401) {
-        throw new Error('invalid api key')
-      }
-      throw new Error(response.statusText)
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'x-api-key': apiKey,
+      'anthropic-version': anthropicVersion,
+      'content-type': 'application/json',
+      'anthropic-dangerous-direct-browser-access': 'true', // https://github.com/anthropics/anthropic-sdk-typescript/pull/504#issuecomment-2306060472
+    },
+    body: JSON.stringify({
+      model: modelId,
+      max_tokens: 1024,
+      messages: [{ role: 'user', content: message }],
+      stream,
+    }),
+  })
+  if (!response.ok) {
+    if (response.status === 401) {
+      throw new Error('invalid api key')
     }
-    const result = await response.json()
-    const responseString = GetResponseString.getResponseString(result)
-    return responseString
-  } catch (error) {
-    return `${error}`
+    throw new Error(response.statusText)
   }
+  if (!response.body) {
+    throw new Error('no response body')
+  }
+  return response.body
 }
--- packages/chat-worker/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/chat-worker/src/parts/HandleSubmit/HandleSubmit.ts
@@ -1,18 +1,40 @@
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 import * as GetChatResponse from '../GetChatResponse/GetChatResponse.ts'
+import * as GetChatResponseStream from '../GetChatResponseStream/GetChatResponseStream.ts'
 
 export const handleSubmit = async (id, input) => {
   const webView = WebViewStates.get(id)
   // TODO race condition
   await webView.port.invoke('addMessage', input)
   await webView.port.invoke('clear')
-  const response = await GetChatResponse.getChatResponse(
+
+  const acc = new WritableStream({
+    async start() {
+      await webView.port.invoke('addMessage', '')
+    },
+
+    async write(message) {
+      await webView.port.invoke('appendMessage', message)
+    },
+  })
+
+  const messageStream = new TransformStream({
+    transform(chunk, controller) {
+      if (chunk && chunk.type && chunk.type === 'content_block_delta') {
+        controller.enqueue(chunk.delta.text)
+      }
+    },
+  })
+
+  const body = await GetChatResponse.getChatResponse(
     input,
     webView.apiKey,
     webView.modelId,
     webView.url,
     webView.anthropicVersion,
+    webView.stream,
   )
-  await webView.port.invoke('addMessage', response)
+  const stream = GetChatResponseStream.getChatResponseStream(body)
+  await stream.pipeThrough(messageStream).pipeTo(acc)
   return undefined
 }

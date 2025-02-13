e7d35049359c3a29505348cb9a0a4e6b8642de5c
Le Vivilet <levivlet@pm.me>
2025-02-13 22:14:25 +0100

    build

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -29,7 +29,7 @@ await bundleJs(
   false,
 )
 
-await bundleJs(join(root, 'extension', 'src', 'chatMain.ts'), join(root, 'dist', 'dist', 'chatMain.js'), false)
+await bundleJs(join(root, 'packages', 'extension', 'src', 'chatMain.ts'), join(root, 'dist', 'dist', 'chatMain.js'), false)
 
 await replace({
   path: join(root, 'dist', 'extension.json'),

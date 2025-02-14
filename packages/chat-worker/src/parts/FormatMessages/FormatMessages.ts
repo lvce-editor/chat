1bf8270e1e5bcbfce64a72d167679355a7f38f27
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 00:31:06 +0100

    feature: support sending images (#102)

--- packages/chat-worker/src/parts/FormatMessages/FormatMessages.ts
+++ packages/chat-worker/src/parts/FormatMessages/FormatMessages.ts
@@ -3,6 +3,29 @@ import type { Message } from '../Message/Message.ts'
 export const formatMessages = (messages: readonly Message[]) => {
   return messages.map((message) => ({
     role: message.role === 'human' ? 'user' : 'assistant',
-    content: Array.isArray(message.content) ? message.content.map((block) => block.content).join('\n') : message.content,
+    content: Array.isArray(message.content)
+      ? message.content.map((block) => {
+          if (block.type === 'image') {
+            return {
+              type: 'image',
+              source: {
+                type: 'base64',
+                media_type: block.mediaType,
+                data: block.content,
+              },
+            }
+          }
+          if (block.type === 'text') {
+            return {
+              type: 'text',
+              text: block.content,
+            }
+          }
+          return {
+            type: 'text',
+            text: block.content,
+          }
+        })
+      : message.content,
   }))
 }

6716cb00ce872a1a59366b498ea4cc5e75041d82
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 00:26:55 +0100

    feature: update message type (#101)

--- packages/chat-worker/src/parts/MessageContent/MessageContent.ts
+++ packages/chat-worker/src/parts/MessageContent/MessageContent.ts
@@ -1,9 +1,21 @@
 // TODO always use object for messages
+export interface BaseMessageContent {
+  readonly type: string
+  readonly content: string
+}
 
-export type MessageContent =
-  | string
-  | Array<{
-      type: 'text' | 'code'
-      content: string
-      language?: string
-    }>
+export interface TextMessageContent extends BaseMessageContent {
+  readonly type: 'text'
+  readonly content: string
+}
+
+export interface CodeMessageContent extends BaseMessageContent {
+  readonly type: 'code'
+  readonly content: string
+  readonly language?: string
+}
+
+export interface ImageMessageContent extends BaseMessageContent {
+  readonly type: 'image'
+  readonly content: string // TODO maybe store the image as blob instead
+}

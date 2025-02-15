cda09c886805d552ac4349888771ec601528be49
Le Vivilet <levivlet@pm.me>
2025-02-15 14:31:52 +0100

    fix

--- packages/chat-worker/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/chat-worker/src/parts/HandleSubmit/HandleSubmit.ts
@@ -1,16 +1,10 @@
 import * as AddMessage from '../AddMessage/AddMessage.ts'
-<<<<<<< HEAD
 import { formatMessagesForApi } from '../FormatMessagesForApi/FormatMessagesForApi.ts'
-=======
 import { formatMessagesForApi } from '../FormatMessages/FormatMessages.ts'
->>>>>>> origin/main
 import * as GetChatResponse from '../GetChatResponse/GetChatResponse.ts'
 import { getNewContent } from '../GetNewContent/GetNewContent.ts'
 import * as HandleApiResponse from '../HandleApiResponse/HandleApiResponse.ts'
-<<<<<<< HEAD
-=======
 import type { Message } from '../Message/Message.ts'
->>>>>>> origin/main
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handleSubmit = async (id: number, input: string) => {
@@ -24,11 +18,7 @@ export const handleSubmit = async (id: number, input: string) => {
   // @ts-ignore
   webView.messages.push(message)
 
-<<<<<<< HEAD
-  await AddMessage.addMessage(id, newContent, 'human')
-=======
   await AddMessage.addMessage(id, message)
->>>>>>> origin/main
 
   const formattedMessages = await formatMessagesForApi(webView.messages)
 
@@ -41,10 +31,5 @@ export const handleSubmit = async (id: number, input: string) => {
     webView.stream,
     webView.maxTokens,
   )
-<<<<<<< HEAD
-  const stream = GetChatResponseStream.getChatResponseStream(body)
-  await HandleApiResponse.handleApiResponse(id, stream)
-=======
   await HandleApiResponse.handleApiResponse(id, body)
->>>>>>> origin/main
 }

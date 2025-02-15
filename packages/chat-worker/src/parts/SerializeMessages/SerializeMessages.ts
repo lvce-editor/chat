cc92bfdfe61ee1c2da85b485ff5dd89749825b53
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 15:35:01 +0100

    feature: add types for saveContent (#141)

--- packages/chat-worker/src/parts/SerializeMessages/SerializeMessages.ts
+++ packages/chat-worker/src/parts/SerializeMessages/SerializeMessages.ts
@@ -1,29 +1,35 @@
 import type { Message } from '../Message/Message.ts'
 import type { MessageContent } from '../MessageContent/MessageContent.ts'
+import type { SavedContent, SavedMessage } from '../SavedMessage/SavedMessage.ts'
 
-const serializeContent = (content: MessageContent) => {
+const serializeContent = (content: MessageContent): SavedContent => {
   switch (content.type) {
     case 'image': {
       return {
         type: content.type,
         mediaType: content.mediaType,
-        fileName: content.file.name,
+        fileName: content.fileName,
       }
     }
     case 'code':
+      return {
+        type: content.type,
+        content: content.content,
+        language: content.language || '',
+      }
     case 'text':
     default:
       return content
   }
 }
 
-const serializeMessage = (message: Message) => {
+const serializeMessage = (message: Message): SavedMessage => {
   return {
     role: message.role,
     content: message.content.map(serializeContent),
   }
 }
 
-export const serializeMessages = (messages: readonly Message[]): readonly any[] => {
+export const serializeMessages = (messages: readonly Message[]): readonly SavedMessage[] => {
   return messages.map(serializeMessage)
 }

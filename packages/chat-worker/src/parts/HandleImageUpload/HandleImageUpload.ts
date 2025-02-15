c69f49caa4dfeeea7c50010a051fc010019ae1b5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 15:04:23 +0100

    feature: use new saveImage function (#133)

--- packages/chat-worker/src/parts/HandleImageUpload/HandleImageUpload.ts
+++ packages/chat-worker/src/parts/HandleImageUpload/HandleImageUpload.ts
@@ -1,28 +1,14 @@
 import type { WebView } from '../WebView/WebView.ts'
 import { createFormContent } from '../CreateFormContent/CreateFormContent.ts'
+import * as SaveImageInCache from '../SaveImageInCache/SaveImageInCache.ts'
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
-
-// save image in cache for restoring image on reload
-const saveImageInCache = async (file: File) => {
-  try {
-    const res = new Response(file, {
-      headers: {
-        'content-type': 'image/png',
-      },
-    })
-    const cacheName = 'chat-image-cache'
-    const cache = await caches.open(cacheName)
-    const url = 'https://example.com/image-1.png'
-    await cache.put(url, res)
-  } catch {}
-}
 
 export const handleImageUpload = async (id: number, file: File | undefined) => {
   const webView = WebViewStates.get(id)
   if (!file) {
     return
   }
-  await saveImageInCache(file)
+  await SaveImageInCache.saveImageInCache(webView.cacheName, webView.cacheBaseUrl, file)
   const blobUrl = await webView.port.invoke('createObjectUrl', file)
   const formContent = createFormContent(blobUrl)
   await webView.port.invoke('updateForm', formContent)

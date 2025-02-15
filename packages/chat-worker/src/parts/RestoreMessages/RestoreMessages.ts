bd70c8a4f32553603ae097456d4e4c00c44268ff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 21:31:05 +0100

    feature: move restoreMessage into separate file (#150)

--- packages/chat-worker/src/parts/RestoreMessages/RestoreMessages.ts
+++ packages/chat-worker/src/parts/RestoreMessages/RestoreMessages.ts
@@ -1,34 +1,5 @@
 import type { Message } from '../Message/Message.ts'
-import { emptyMessage } from '../EmptyMessage/EmptyMessage.ts'
-import * as RestoreImage from '../RestoreImage/RestoreImage.ts'
-
-const restoreContent = async (id: number, cacheName: string, cacheBaseUrl: string, savedContent: any) => {
-  if (savedContent.type === 'image') {
-    return RestoreImage.restoreImage(id, cacheName, cacheBaseUrl, savedContent)
-  }
-  return savedContent
-}
-
-const restoreMessage = async (id: number, cacheName: string, cacheBaseUrl: string, savedMessage: any): Promise<Message> => {
-  if (!savedMessage) {
-    return emptyMessage
-  }
-  if (!savedMessage.content) {
-    return emptyMessage
-  }
-  if (!Array.isArray(savedMessage.content)) {
-    return emptyMessage
-  }
-  return {
-    ...savedMessage,
-    webViewId: id,
-    content: await Promise.all(
-      savedMessage.content.map((content) => {
-        return restoreContent(id, cacheName, cacheBaseUrl, content)
-      }),
-    ),
-  }
-}
+import * as RestoreMessage from '../RestoreMessage/RestoreMessage.ts'
 
 export const restoreMessages = async (
   id: number,
@@ -39,7 +10,7 @@ export const restoreMessages = async (
   const baseMessages = savedState?.messages || []
   const restored1 = await Promise.all(
     baseMessages.map((baseMessage) => {
-      return restoreMessage(id, cacheName, cacheBaseUrl, baseMessage)
+      return RestoreMessage.restoreMessage(id, cacheName, cacheBaseUrl, baseMessage)
     }),
   )
   return restored1

e698c1c30910fbd97b36bb81e88946d153708c9d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 14:14:07 +0100

    feature: move api response into separate file (#124)

---
+++ packages/chat-worker/src/parts/HandleApiResponse/HandleApiResponse.ts
@@ -0,0 +1,51 @@
+import * as AddMessage from '../AddMessage/AddMessage.ts'
+import * as FormatMessage from '../FormatMessage/FormatMessage.ts'
+import * as GetChatResponseStream from '../GetChatResponseStream/GetChatResponseStream.ts'
+import * as RenderMessage from '../RenderMessage/RenderMessage.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+
+export const handleApiResponse = async (id: number, body: ReadableStream) => {
+  const webView = WebViewStates.get(id)
+
+  let currentMessage = ''
+  const acc = new WritableStream({
+    async start() {
+      await AddMessage.addMessage(
+        id,
+        [
+          {
+            type: 'text',
+            content: '',
+          },
+        ],
+        'ai',
+      )
+    },
+
+    async write(message) {
+      currentMessage += message
+      const blocks = FormatMessage.formatMessage(currentMessage)
+      const messageVDom = RenderMessage.renderMessage(blocks, 'ai')
+      await webView.port.invoke('updateMessage', messageVDom)
+    },
+
+    async close() {
+      // Add AI message to state once complete
+      // @ts-ignore
+      webView.messages.push({
+        role: 'ai',
+        content: FormatMessage.formatMessage(currentMessage),
+      })
+    },
+  })
+
+  const messageStream = new TransformStream({
+    transform(chunk, controller) {
+      if (chunk && chunk.type && chunk.type === 'content_block_delta') {
+        controller.enqueue(chunk.delta.text)
+      }
+    },
+  })
+  const stream = GetChatResponseStream.getChatResponseStream(body)
+  await stream.pipeThrough(messageStream).pipeTo(acc)
+}

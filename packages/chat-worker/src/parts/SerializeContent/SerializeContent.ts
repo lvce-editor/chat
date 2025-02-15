a61b57877b958c9f65d8b66c37d9f412f37a898d
Le Vivilet <levivlet@pm.me>
2025-02-15 21:31:44 +0100

    feature: move serializeMessage into separate file

---
+++ packages/chat-worker/src/parts/SerializeContent/SerializeContent.ts
@@ -0,0 +1,23 @@
+import type { MessageContent } from '../MessageContent/MessageContent.ts'
+import type { SavedContent } from '../SavedMessage/SavedMessage.ts'
+
+export const serializeContent = (content: MessageContent): SavedContent => {
+  switch (content.type) {
+    case 'image': {
+      return {
+        type: content.type,
+        mediaType: content.mediaType,
+        fileName: content.fileName,
+      }
+    }
+    case 'code':
+      return {
+        type: content.type,
+        content: content.content,
+        language: content.language || '',
+      }
+    case 'text':
+    default:
+      return content
+  }
+}
--- packages/chat-worker/src/parts/SerializeMessages/SerializeMessages.ts
+++ packages/chat-worker/src/parts/SerializeMessages/SerializeMessages.ts
@@ -1,35 +1,7 @@
 import type { Message } from '../Message/Message.ts'
-import type { MessageContent } from '../MessageContent/MessageContent.ts'
-import type { SavedContent, SavedMessage } from '../SavedMessage/SavedMessage.ts'
-
-const serializeContent = (content: MessageContent): SavedContent => {
-  switch (content.type) {
-    case 'image': {
-      return {
-        type: content.type,
-        mediaType: content.mediaType,
-        fileName: content.fileName,
-      }
-    }
-    case 'code':
-      return {
-        type: content.type,
-        content: content.content,
-        language: content.language || '',
-      }
-    case 'text':
-    default:
-      return content
-  }
-}
-
-const serializeMessage = (message: Message): SavedMessage => {
-  return {
-    role: message.role,
-    content: message.content.map(serializeContent),
-  }
-}
+import type { SavedMessage } from '../SavedMessage/SavedMessage.ts'
+import * as SerializeMessage from '../SerializeMessage/SerializeMessage.ts'
 
 export const serializeMessages = (messages: readonly Message[]): readonly SavedMessage[] => {
-  return messages.map(serializeMessage)
+  return messages.map(SerializeMessage.serializeMessage)
 }

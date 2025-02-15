2ab3775234b4072f2844b47b7157477f86f84dc3
Le Vivilet <levivlet@pm.me>
2025-02-15 22:01:47 +0100

    feature: remove unused code

--- packages/chat-worker/src/parts/GetResponseString/GetResponseString.ts
+++
@@ -1,9 +0,0 @@
-export const getResponseString = (result) => {
-  const {content} = result
-
-  if (content.length === 0) {
-    return 'empty'
-  }
-  const first = content[0]
-  return first.text
-}

f1a3177c81b37da926088460209c6e6ce4a1ec53
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-15 14:56:42 +0100

    feature: add getImageFromCache function (#132)

---
+++ packages/chat-worker/src/parts/SaveImageInCache/SaveImageInCache.ts
@@ -0,0 +1,13 @@
+// save image in cache for restoring image on reload
+export const saveImageInCache = async (cacheName: string, cacheBaseUrl: string, file: File) => {
+  try {
+    const res = new Response(file, {
+      headers: {
+        'content-type': 'image/png',
+      },
+    })
+    const cache = await caches.open(cacheName)
+    const url = `${cacheBaseUrl}/${file.name}`
+    await cache.put(url, res)
+  } catch {}
+}

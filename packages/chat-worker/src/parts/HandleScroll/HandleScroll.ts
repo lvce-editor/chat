39eacb4772bc02d537f1b3904dd991543c126162
Le Vivilet <levivlet@pm.me>
2025-02-17 21:59:18 +0100

    fix

--- packages/chat-worker/src/parts/HandleScroll/HandleScroll.ts
+++ packages/chat-worker/src/parts/HandleScroll/HandleScroll.ts
@@ -1,9 +1,12 @@
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handleScroll = async (id: number, scrollOffset: number) => {
+  const webView = WebViewStates.get(id)
+  const isScrolledToBottom = await webView.port.invoke('checkIsBottom')
+
   await WebViewStates.update(id, (webView) => ({
     ...webView,
     scrollOffset,
-    isScrolledToBottom: scrollOffset >= document.documentElement.scrollHeight - document.documentElement.clientHeight,
+    isScrolledToBottom,
   }))
 }

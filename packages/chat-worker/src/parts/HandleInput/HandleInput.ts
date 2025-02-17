a9bbeaa007922259bc5df00f11f4897f62b59963
Le Vivilet <levivlet@pm.me>
2025-02-17 22:23:21 +0100

    add source

--- packages/chat-worker/src/parts/HandleInput/HandleInput.ts
+++ packages/chat-worker/src/parts/HandleInput/HandleInput.ts
@@ -1,7 +1,9 @@
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const handleInput = async (id: number, input: string): Promise<void> => {
   await WebViewStates.update(id, {
     currentInput: input,
+    inputSource: InputSource.User,
   })
 }

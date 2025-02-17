06e1d167d5e9ff33e95cbc447cefb51123ee70b8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-17 23:19:26 +0100

    feature: render dom first before value (#195)

--- packages/chat-worker/src/parts/Diff/Diff.ts
+++ packages/chat-worker/src/parts/Diff/Diff.ts
@@ -4,11 +4,11 @@ import * as InputSource from '../InputSource/InputSource.ts'
 
 export const diff = (a: WebView, b: WebView): readonly number[] => {
   const diffs: number[] = []
-  if (b.inputSource === InputSource.Script && a.currentInput !== b.currentInput) {
-    diffs.push(DiffType.InputValue)
-  }
   if (a.messages !== b.messages || a.images !== b.images || a.previewImageUrl !== b.previewImageUrl) {
     diffs.push(DiffType.Dom)
+  }
+  if (b.inputSource === InputSource.Script && a.currentInput !== b.currentInput) {
+    diffs.push(DiffType.InputValue)
   }
   return diffs
 }

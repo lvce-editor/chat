ddec6b60ae48fa4bf7c8d85846efb4bac4d550fb
Le Vivilet <levivlet@pm.me>
2025-02-17 21:50:16 +0100

    feature: add functional render function

---
+++ packages/chat-worker/src/parts/Render/Render.ts
@@ -0,0 +1,50 @@
+import type { WebView } from '../WebView/WebView.ts'
+import type { VirtualElement } from '../VirtualDom/VirtualDom.ts'
+import { createMessageViewModel } from '../CreateMessageViewModel/CreateMessageViewModel.ts'
+import { renderMessage } from '../RenderMessage/RenderMessage.ts'
+import { createFormContent } from '../CreateFormContent/CreateFormContent.ts'
+
+export const render = async (webView: WebView): Promise<VirtualElement> => {
+  const viewModels = await Promise.all(webView.messages.map(createMessageViewModel))
+  const messageVDoms = viewModels.map(renderMessage)
+
+  return {
+    type: 'div',
+    className: 'App',
+    children: [
+      {
+        type: 'div',
+        className: 'Header',
+        children: [
+          {
+            type: 'button',
+            className: 'NewChatButton',
+            textContent: 'New Chat',
+            events: {
+              click: 'handleNewChat',
+            },
+          },
+        ],
+      },
+      {
+        type: 'div',
+        className: 'ContentWrapper',
+        children: [
+          {
+            type: 'div',
+            className: 'Output',
+            children: messageVDoms,
+          },
+        ],
+      },
+      {
+        type: 'form',
+        className: 'Form',
+        events: {
+          submit: 'handleSubmit',
+        },
+        children: [createFormContent(webView.previewImageUrl)],
+      },
+    ],
+  }
+}

7917446eacd336f27b81ec594371abeae55adacd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-17 20:20:12 +0100

    feature: improve api error handling (#181)
    * feature: improve api error handling
    
    * tsc

--- packages/chat-worker/src/parts/UnwrapApiResponse/UnwrapApiResponse.ts
+++ packages/chat-worker/src/parts/UnwrapApiResponse/UnwrapApiResponse.ts
@@ -1,16 +1,21 @@
+import { ApiError } from '../ApiError/ApiError.ts'
+import * as ErrorCodes from '../ErrorCodes/ErrorCodes.ts'
 import { parseErrorMessage } from '../ParseErrorMessage/ParseErrorMessage.ts'
 
 export const unwrapApiResponse = async (response: Response): Promise<ReadableStream<Uint8Array<ArrayBufferLike>>> => {
   if (!response.ok) {
     if (response.status === 401) {
-      throw new Error('invalid api key')
+      throw new ApiError('Invalid API key', ErrorCodes.E_INVALID_API_KEY)
     }
     const errorMessage = await parseErrorMessage(response)
+    if (errorMessage.includes('does not support image input')) {
+      throw new ApiError(errorMessage, ErrorCodes.E_SONNET_DOES_NOT_SUPPORT_IMAGE_UPLOAD)
+    }
     throw new Error(errorMessage)
   }
 
   if (!response.body) {
-    throw new Error('no response body')
+    throw new ApiError('No response body', ErrorCodes.E_NO_RESPONSE_BODY)
   }
   return response.body
 }

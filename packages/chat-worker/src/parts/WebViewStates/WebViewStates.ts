eba23968d8e9c688aa17510562afbbb73ad4a509
Le Vivilet <levivlet@pm.me>
2025-02-17 22:18:15 +0100

    feature: add webview update function

--- packages/chat-worker/src/parts/WebViewStates/WebViewStates.ts
+++ packages/chat-worker/src/parts/WebViewStates/WebViewStates.ts
@@ -1,4 +1,5 @@
 import type { WebView } from '../WebView/WebView.ts'
+import { render } from '../Render/Render.ts'
 
 const webViews = Object.create(null)
 
@@ -8,4 +9,28 @@ export const get = (id: number): WebView => {
 
 export const set = (id: number, webView: WebView) => {
   webViews[id] = webView
+}
+
+const deepEqual = (a: unknown, b: unknown): boolean => {
+  return JSON.stringify(a) === JSON.stringify(b)
+}
+
+export const update = async (id: number, newWebView: Partial<WebView>) => {
+  const oldWebView = get(id)
+  const updatedWebView = {
+    ...oldWebView,
+    ...newWebView,
+  }
+  set(id, updatedWebView)
+
+  // Render new VDOM
+  const newVdom = await render(updatedWebView)
+  const oldVdom = await render(oldWebView)
+
+  // Skip render if vdom hasn't changed
+  if (deepEqual(oldVdom, newVdom)) {
+    return
+  }
+
+  await updatedWebView.port.invoke('render', newVdom)
 }

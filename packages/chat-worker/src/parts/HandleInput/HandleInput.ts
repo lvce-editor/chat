5d643d42c1a047ce276acb0ce3258203040dcaa8
Le Vivilet <levivlet@pm.me>
2025-02-17 22:22:44 +0100

    fix

--- packages/chat-worker/src/parts/HandleInput/HandleInput.ts
+++ packages/chat-worker/src/parts/HandleInput/HandleInput.ts
@@ -1,7 +1,6 @@
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handleInput = async (id: number, input: string): Promise<void> => {
-  console.log({ input })
   await WebViewStates.update(id, {
     currentInput: input,
   })

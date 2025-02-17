f02c1b778013c51cccc8db6e474090552b64789b
Le Vivilet <levivlet@pm.me>
2025-02-17 23:12:03 +0100

    add diffing

--- packages/chat-worker/src/parts/WebViewStates/WebViewStates.ts
+++ packages/chat-worker/src/parts/WebViewStates/WebViewStates.ts
@@ -1,7 +1,6 @@
+import * as Diff from '../Diff/Diff.ts'
+import * as GetRenderCommands from '../GetRenderCommands/GetRenderCommands.ts'
 import type { WebView } from '../WebView/WebView.ts'
-import * as DeepEqual from '../DeepEqual/DeepEqual.ts'
-import * as InputSource from '../InputSource/InputSource.ts'
-import * as Render from '../Render/Render.ts'
 
 const webViews = Object.create(null)
 
@@ -15,27 +14,23 @@ export const set = (id: number, webView: WebView) => {
 
 export const update = async (id: number, newWebView: Partial<WebView>) => {
   const oldWebView = get(id)
-  const updatedWebView = {
+  const updatedWebView: WebView = {
     ...oldWebView,
     ...newWebView,
   }
   set(id, updatedWebView)
 
-  // Update input value if source is script
-  if (updatedWebView.inputSource === InputSource.Script && oldWebView.currentInput !== newWebView.currentInput) {
-    await updatedWebView.port.invoke('setValue', 'Input', updatedWebView.currentInput)
-  }
-
-  // Render new VDOM
-  const newVdom = await Render.render(updatedWebView)
-  const oldVdom = await Render.render(oldWebView)
-
-  // Skip render if vdom hasn't changed
-  if (DeepEqual.deepEqual(oldVdom, newVdom)) {
+  const diffs = Diff.diff(oldWebView, updatedWebView)
+  if (diffs.length === 0) {
     return
   }
 
-  console.log({ oldVdom, newVdom })
+  const commands = await GetRenderCommands.getRenderCommands(oldWebView, updatedWebView, diffs)
 
-  await updatedWebView.port.invoke('render', newVdom)
+  for (const item of commands) {
+    if (item.length === 0) {
+      continue
+    }
+    await updatedWebView.port.invoke(...item)
+  }
 }

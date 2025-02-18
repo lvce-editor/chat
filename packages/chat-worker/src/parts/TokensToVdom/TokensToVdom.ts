dee203601cf6a635a1e13ac94ff1242759474d02
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-18 21:49:59 +0100

    feature: improve tokensToVdom function (#204)

--- packages/chat-worker/src/parts/TokensToVdom/TokensToVdom.ts
+++ packages/chat-worker/src/parts/TokensToVdom/TokensToVdom.ts
@@ -1,14 +1,15 @@
 import type { Token } from '../Token/Token.ts'
+import type { VirtualElement } from '../VirtualDom/VirtualDom.ts'
+
+const tokenToVdom = (token: Token): VirtualElement => {
+  return {
+    type: 'span',
+    className: `Token Token${token.type}`,
+    children: [token.text],
+  }
+}
 
 // Then you could convert tokens to vdom like this:
 export const tokensToVDOM = (tokens: readonly Token[]): readonly any[] => {
-  return tokens.map((token) => {
-    return {
-      type: 'span',
-      props: {
-        className: `token-${token.type}`,
-      },
-      children: [token.text],
-    }
-  })
+  return tokens.map(tokenToVdom)
 }

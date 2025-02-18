32b3c9e7b2ac4502cfdf9b5205d46e5ac8e1f06a
Le Vivilet <levivlet@pm.me>
2025-02-18 21:34:17 +0100

    feature: split content view model into separate functions

--- packages/chat-worker/src/parts/CreateMessageContentViewModel/CreateMessageContentViewModel.ts
+++ packages/chat-worker/src/parts/CreateMessageContentViewModel/CreateMessageContentViewModel.ts
@@ -1,39 +1,56 @@
-import type { MessageContent } from '../MessageContent/MessageContent.ts'
+import type {
+  CodeMessageContent,
+  ImageMessageContent,
+  MessageContent,
+  TextMessageContent,
+} from '../MessageContent/MessageContent.ts'
 import type { MessageBlockViewModel } from '../MessageViewModel/MessageViewModel.ts'
 import * as IsFile from '../IsFile/IsFile.ts'
 
-export const createMessageContentViewModel = async (part: MessageContent, webView: any): Promise<MessageBlockViewModel> => {
-  if (part.type === 'image') {
-    if (!IsFile.isFile(part.file)) {
-      return {
-        type: 'image',
-        content: '',
-        display: {
-          blobUrl: '#',
-        },
-      }
-    }
-    const blobUrl = await webView.port.invoke('createObjectUrl', part.file)
+const createMessageContentImageViewModel = async (part: ImageMessageContent, webView: any): Promise<MessageBlockViewModel> => {
+  if (!IsFile.isFile(part.file)) {
     return {
       type: 'image',
       content: '',
       display: {
-        blobUrl,
+        blobUrl: '#',
       },
     }
   }
-  if (part.type === 'code') {
-    return {
-      type: 'code',
-      content: part.content,
-      display: {
-        language: part.language,
-      },
-    }
+  const blobUrl = await webView.port.invoke('createObjectUrl', part.file)
+  return {
+    type: 'image',
+    content: '',
+    display: {
+      blobUrl,
+    },
   }
+}
+
+const createMessageContentCodeViewModel = (part: CodeMessageContent): MessageBlockViewModel => {
+  return {
+    type: 'code',
+    content: part.content,
+    display: {
+      language: part.language,
+    },
+  }
+}
+
+const createMessageContentTextViewModel = (part: TextMessageContent): MessageBlockViewModel => {
   return {
     type: 'text',
     content: part.content,
     display: {},
   }
+}
+
+export const createMessageContentViewModel = async (part: MessageContent, webView: any): Promise<MessageBlockViewModel> => {
+  if (part.type === 'image') {
+    return createMessageContentImageViewModel(part, webView)
+  }
+  if (part.type === 'code') {
+    return createMessageContentCodeViewModel(part)
+  }
+  return createMessageContentTextViewModel(part)
 }

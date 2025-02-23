cc09ea6cdc29abcd14602dd0796fd79c5afa3ddd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-23 12:03:41 +0100

    feature: improve webview test (#242)

--- packages/e2e/src/chat.ts
+++ packages/e2e/src/chat.ts
@@ -2,12 +2,16 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'chat'
 
-export const test: Test = async ({ Main, Locator, expect }) => {
+export const test: Test = async ({ Main, Locator, expect, WebView }) => {
   // act
   await Main.openUri('test://example.chat')
 
   // assert
-  const webview = Locator('.WebViewIframe')
-  await expect(webview).toBeVisible()
-  await expect(webview).toHaveAttribute('title', 'Chat')
+  const webviewElement = Locator('.WebViewIframe')
+  await expect(webviewElement).toBeVisible()
+  await expect(webviewElement).toHaveAttribute('title', 'Chat')
+
+  // const webView = await WebView.fromId('builtin.chat')
+  // const button = webView.locator('.NewChatButton')
+  // await expect(button).toBeVisible()
 }

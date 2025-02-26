3e7e364cf6610ab1589b81d6ac0fe867040215f6
Le Vivilet <levivlet@pm.me>
2025-02-26 20:10:15 +0100

    feature: enable image url cache

--- packages/chat-worker/src/parts/HandleNewChat/HandleNewChat.ts
+++ packages/chat-worker/src/parts/HandleNewChat/HandleNewChat.ts
@@ -1,11 +1,21 @@
 import * as InputSource from '../InputSource/InputSource.ts'
 import * as Update from '../Update/Update.ts'
+import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
 export const handleNewChat = async (id: number) => {
+  const webView = WebViewStates.get(id)
+
+  // TODO maybe can revoke url directly after image has been displayed
+  // Cleanup old URLs
+  for (const url of webView.imageUrlCache.values()) {
+    await webView.port.invoke('revokeObjectUrl', url)
+  }
+
   await Update.update(id, {
     messages: [],
     currentInput: '',
     images: [],
+    imageUrlCache: new Map(),
     previewImageUrl: '',
     focused: true,
     inputSource: InputSource.Script,

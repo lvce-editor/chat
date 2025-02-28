9141cce706e4623763f41e537361d47180d43ec2
Le Vivilet <levivlet@pm.me>
2025-02-28 22:05:59 +0100

    feature: add getRenderCommands test

---
+++ packages/chat-worker/test/GetRenderCommands.test.ts
@@ -0,0 +1,250 @@
+import { test, expect } from '@jest/globals'
+import * as GetRenderCommands from '../src/parts/GetRenderCommands/GetRenderCommands.ts'
+import * as DiffType from '../src/parts/DiffType/DiffType.ts'
+import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import type { WebView } from '../src/parts/WebView/WebView.ts'
+
+test('returns empty array when no diffs', async () => {
+  const oldWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.User,
+    images: [],
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    imageUrlCache: new Map(),
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const newWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.User,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const diffs: number[] = []
+
+  const result = await GetRenderCommands.getRenderCommands(oldWebView, newWebView, diffs)
+  expect(result).toEqual([])
+})
+
+test('returns render command for DOM diff', async () => {
+  const oldWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.User,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const newWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.User,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const diffs = [DiffType.Dom]
+
+  const result = await GetRenderCommands.getRenderCommands(oldWebView, newWebView, diffs)
+  expect(result).toEqual([['render', expect.any(Object)]])
+})
+
+test('returns setValue command for input value diff', async () => {
+  const oldWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.Script,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const newWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: 'Hello',
+    focused: false,
+    inputSource: InputSource.Script,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const diffs = [DiffType.InputValue]
+
+  const result = await GetRenderCommands.getRenderCommands(oldWebView, newWebView, diffs)
+  expect(result).toEqual([['setValue', 'Input', 'Hello']])
+})
+
+test('returns focus command for focus diff', async () => {
+  const oldWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.Script,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const newWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: true,
+    inputSource: InputSource.Script,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const diffs = [DiffType.Focus]
+
+  const result = await GetRenderCommands.getRenderCommands(oldWebView, newWebView, diffs)
+  expect(result).toEqual([['focusInput']])
+})
+
+test('returns multiple commands for multiple diffs', async () => {
+  const oldWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: '',
+    focused: false,
+    inputSource: InputSource.Script,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const newWebView: WebView = {
+    anthropicVersion: '',
+    apiKey: '',
+    cacheBaseUrl: '',
+    cacheName: '',
+    messages: [],
+    currentInput: 'Hello',
+    focused: true,
+    inputSource: InputSource.Script,
+    images: [],
+    imageUrlCache: new Map(),
+    isScrolledToBottom: false,
+    maxTokens: 0,
+    modelId: '',
+    port: 0,
+    previewImageUrl: '',
+    scrollOffset: 0,
+    stream: false,
+    time: 0,
+    url: '',
+  }
+  const diffs = [DiffType.Dom, DiffType.InputValue, DiffType.Focus]
+
+  const result = await GetRenderCommands.getRenderCommands(oldWebView, newWebView, diffs)
+  expect(result).toEqual([['render', expect.any(Object)], ['setValue', 'Input', 'Hello'], ['focusInput']])
+})

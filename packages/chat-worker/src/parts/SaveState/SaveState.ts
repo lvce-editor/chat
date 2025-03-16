bf43128c4ff5131725ef64dd86c76ba69e0c8b8c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-16 22:52:53 +0100

    feature: use input id for saving state (#279)

--- packages/chat-worker/src/parts/SaveState/SaveState.ts
+++ packages/chat-worker/src/parts/SaveState/SaveState.ts
@@ -1,10 +1,11 @@
 import type { SavedState } from '../SavedState/SavedState.ts'
-import { id } from '../Id/Id.ts'
+import * as Id from '../Id/Id.ts'
 import { serializeMessages } from '../SerializeMessages/SerializeMessages.ts'
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
-export const saveState = (): SavedState => {
-  const webView = WebViewStates.get(id)
+export const saveState = (id: number): SavedState => {
+  const actualId = id || Id.id
+  const webView = WebViewStates.get(actualId)
   const { messages, scrollOffset, currentInput, focused, inputSource } = webView
   return {
     messages: serializeMessages(messages),

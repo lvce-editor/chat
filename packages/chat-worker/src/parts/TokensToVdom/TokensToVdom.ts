c8043a38f92b5da763dd1430dcdc3c971c9d261c
Le Vivilet <levivlet@pm.me>
2025-04-24 21:24:26 +0200

    feature: move vdom into separate file

--- packages/chat-worker/src/parts/TokensToVdom/TokensToVdom.ts
+++ packages/chat-worker/src/parts/TokensToVdom/TokensToVdom.ts
@@ -1,15 +1,6 @@
 import type { Token } from '../Token/Token.ts'
-import type { VirtualElement } from '../VirtualDom/VirtualDom.ts'
+import { tokenToVdom } from '../TokenToVdom/TokenToVdom.ts'
 
-const tokenToVdom = (token: Token): VirtualElement => {
-  return {
-    type: 'span',
-    className: `Token Token${token.type}`,
-    children: [token.text],
-  }
-}
-
-// Then you could convert tokens to vdom like this:
 export const tokensToVDOM = (tokens: readonly Token[]): readonly any[] => {
   return tokens.map(tokenToVdom)
 }

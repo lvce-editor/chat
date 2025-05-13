423881dbb1dc5279d875f88c29566d3999b7dd2a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-13 20:55:49 +0200

    feature: increase limit (#304)

--- packages/chat-worker/src/parts/HandleSubmit/HandleSubmit.ts
+++ packages/chat-worker/src/parts/HandleSubmit/HandleSubmit.ts
@@ -11,7 +11,7 @@ import * as UnwrapApiResponse from '../UnwrapApiResponse/UnwrapApiResponse.ts'
 import * as Update from '../Update/Update.ts'
 import * as WebViewStates from '../WebViewStates/WebViewStates.ts'
 
-const max = 25 // prevent endless loop
+const max = 50 // prevent endless loop
 let current = 0
 
 export const handleSubmit = async (id: number) => {

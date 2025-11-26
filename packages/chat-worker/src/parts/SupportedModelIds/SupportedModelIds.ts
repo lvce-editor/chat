d001532251a885fe2947c610fcedc9adf029f741
Le Vivilet <levivilet@pm.me>
2025-11-27 00:33:53 +0100

    feature: move models into separate file (#335)

--- packages/chat-worker/src/parts/SupportedModelIds/SupportedModelIds.ts
+++ packages/chat-worker/src/parts/SupportedModelIds/SupportedModelIds.ts
@@ -1,8 +1,3 @@
-export const supportedModelIds = [
-  'claude-3-7-sonnet-20250219',
-  'claude-3-5-sonnet-20241022',
-  'claude-3-5-sonnet-20240620',
-  'claude-3-5-haiku-20241022',
-  'claude-3-opus-20240229',
-  'claude-3-haiku-20240307',
-]
+import { models } from '../Models/Models.ts'
+
+export const supportedModelIds = models.map((model) => model.id)

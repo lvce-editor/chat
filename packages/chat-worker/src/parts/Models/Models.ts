eac42a1cda64310a7e64b192abd73ef5d22c1706
Le Vivilet <levivilet@pm.me>
2025-11-27 00:28:43 +0100

    feature: move models into separate file

---
+++ packages/chat-worker/src/parts/Models/Models.ts
@@ -0,0 +1,26 @@
+export const models = [
+  {
+    name: 'Claude 3.7 Sonnet',
+    id: 'claude-3-7-sonnet-20250219',
+  },
+  {
+    name: 'Claude 3.5 Sonnet',
+    id: 'claude-3-5-sonnet-20241022',
+  },
+  {
+    name: 'Claude 3.5 Sonnet (June 2024)',
+    id: 'claude-3-5-sonnet-20240620',
+  },
+  {
+    name: 'Claude 3.5 Haiku',
+    id: 'claude-3-5-haiku-20241022',
+  },
+  {
+    name: 'Claude 3 Opus',
+    id: 'claude-3-opus-20240229',
+  },
+  {
+    name: 'Claude 3 Haiku (March 2024)',
+    id: 'claude-3-haiku-20240307',
+  },
+]
